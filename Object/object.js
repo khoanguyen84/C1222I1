5765f2816423ac6e62f69a7785e71018734456aa
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-01-09 09:47:38 +0700

    object

---
+++ Object/object.js
@@ -0,0 +1,13 @@
+let nhat = {
+    name: "Nhật",
+    gender: true,
+    email: "nhat@gmail.com",
+    age: 20,
+    hobbies: ["Đá banh", "Cầu lông"]
+}
+
+let nhat_2 = ["Nhật", true, 20, "nhat@gmail.com", ["Đá banh", "Cầu lông"]];
+
+console.log(nhat_2[2]);
+console.log(nhat["age"]);
+console.log(nhat.age);
